77f35241c62fb7ff22f5f0e3fbde1728a7899f2e
dvbrown <dvbrown@student.unimelb.edu.au>
2013-09-17 14:08:41 +1000

    separate pipeline taks from ruffus config

---
+++ Bioinformatics/Ruffus/MyPipeline/tasks.py
@@ -0,0 +1,70 @@
+
+#Hard code reference file locations for aligner. Change for Merri. human_g1k_v37.rev.1.bt2
+refGenome = '/vlsci/VR0002/shared/Reference_Files/Indexed_Ref_Genomes/bowtie_Indexed/human_g1k_v37'
+
+
+#def trimmomatic(read1, outFiles):
+#    read2 = re.sub('R1','R2', read1)
+#    #split the output and touchFile
+#    trimRead1, flagFile = outFiles
+#    trimRead2 = re.sub('R1','R2', trimRead1)
+#    unpair1 = read1 + 'unpair'
+#    unpair2 = read2 + 'unpair'
+#    headParams = 'java -Xmx4g -classpath ' 
+#    classPath = '/Users/d.brown6/Bioinformatics/Trimmomatic-0.22/trimmomatic-0.22.jar '
+#    trimOptions = 'org.usadellab.trimmomatic.TrimmomaticPE -threads 1 -phred33 -trimlog ' + read1 + '.trimLog.txt '
+#    trailParams = ' ILLUMINACLIP:/Users/d.brown6/Bioinformatics/Trimmomatic-0.22/IlluminaAdaptersCustom.fa:2:40:15 LEADING:20 TRAILING:20 MINLEN:100'
+# 
+#    #------------------------------build shell command-------------------------------------  
+#    commTrim = headParams + classPath + trimOptions + read1 + ' ' + read2 +\
+#    ' ' + trimRead1 + ' ' + unpair1 + ' ' + trimRead2 + ' ' + unpair2 + ' ' + trailParams
+#    #--------------------------------------------------------------------------------------
+#    
+#    started = time.strftime('%X %x %Z')
+#    print 'running task trimmomatic at {0}'.format(started)
+#    print commTrim
+#    #run the command
+#    #os.system(commTrim)
+#
+#    #touch file indicates success. It should be empty if there was success 
+#    finished = time.strftime('%X %x %Z')
+#    open(flagFile , 'w').write(finished)
+#
+#unzipInput = options.input_file
+#
+#@transform(unzipInput, suffix('.gz'), [r'../trimFastq/\1.fastq', 'unzipSuccess.txt'])
+#def unzip(input1, outFiles):
+#    input2 = re.sub('R1','R2', input1)
+#    output, flagFile = outFiles
+#    comm = 'gunzip ' + input1 + ' ' + input2
+#    print comm
+#    os.system(comm) 
+#    #touch file indicates success. It should be have the completion time if there was success 
+#    finished = time.strftime('%X %x %Z')
+#    open(flagFile , 'w').write(finished)
+
+
+#@follows(unzip)
+@transform(alignInput, suffix(".fastq"), [r'bowtie2Align/\1.bowtie.bam', ".alignSuccess.txt"])
+def bowtie2(read1, outFiles):
+    'Emit the aligned files in the bowtie2AlignDirectory. Used local mode with default settigs. Pipe output to samtools to produce a sorted bam file'
+    read2 = re.sub('R1','R2', read1)
+    rgID = read1[0:7]
+    #split the output and touchFile
+    output, flagFile = outFiles
+    #------------------------------build shell command--------------------------------------
+    alignNotes = "Local alignment (soft-clipping), report best alignment"
+    headParams = 'bowtie2 --local -p 8 --rg-id ' + rgID
+    midParams = ' -x ' + refGenome + ' -1 ' + read1 + ' -2 ' + read2
+    tailParams = ' | samtools view -bS -o ' + output + ' -'
+    comm = headParams + midParams + tailParams
+    #---------------------------------------------------------------------------------------  
+    started = time.strftime('%X %x %Z')
+    print 'running task trimmomatic at {0}'.format(started)
+    print comm
+    #run the command
+    #os.system(comm)
+    #touch file indicates success. It should be have the completion time if there was success 
+    finished = time.strftime('%X %x %Z')
+    open(flagFile , 'w').write(finished)
+    
