f26761bd352744bb6daef145f167f0ea71a81245
dvbrown <dvbrown@student.unimelb.edu.au>
2013-10-07 14:30:24 +1100

    quoted pipes using repr
    I think this is unecessary

--- Bioinformatics/Ruffus/MyPipeline/htSeq.py
+++ Bioinformatics/Ruffus/MyPipeline/htSeq.py
@@ -1,9 +1,13 @@
+refTranscripts = '/vlsci/VR0002/shared/Reference_Files/human_UCSC_genes_v37_nochrprefix.gtf'
+from tasks import runJob
+
 def htSeq(bamFile, outFiles):
     output, flagFile = outFiles
+    # The input needs to be sorted by readname. Pipe from samtools to HTSeq
     #------------------------------build shell command--------------------------------------
-    headParams = '' 
-    tailParams = ''
-    midParams = ''
-    comm = headParams + bamFile + ' OUTPUT=' + output + midParams + tailParams
+    headParams = 'samtools sort -no ' + bamFile +  repr(' - | samtools view -h |')
+    tailParams = 'python -m HTSeq.scripts.count  --stranded=yes -m intersection-nonempty'
+    midParams = repr(' - ') + refTranscripts + repr(' > ') + output
+    comm = headParams + midParams + tailParams
     #---------------------------------------------------------------------------------------  
     runJob(comm, 'htSeq', flagFile)
