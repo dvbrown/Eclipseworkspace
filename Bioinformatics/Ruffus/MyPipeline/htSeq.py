70886102daa119938e376120564210d23f5b5645
dvbrown <dvbrown@student.unimelb.edu.au>
2013-10-07 17:35:14 +1100

    extending file with bedToos

--- Bioinformatics/Ruffus/MyPipeline/htSeq.py
+++ Bioinformatics/Ruffus/MyPipeline/htSeq.py
@@ -1,4 +1,5 @@
 refTranscripts = '/vlsci/VR0002/shared/Reference_Files/human_UCSC_genes_v37_nochrprefix.gtf'
+rRnaBedFile = 'someFile' #Find the rRNA bed file
 from tasks import runJob
 
 def htSeq(bamFile, outFiles):
@@ -11,3 +12,21 @@ def htSeq(bamFile, outFiles):
     comm = headParams + midParams + tailParams
     #---------------------------------------------------------------------------------------  
     runJob(comm, 'htSeq', flagFile)
+    
+    
+def makeBED(bamFile, outFiles):
+    output, flagFile = outFiles
+    # The input needs to be sorted by readname. Pipe from samtools to HTSeq
+    #------------------------------build shell command--------------------------------------
+    comm = 'bamToBed -bedpe -i ' + bamFile + ' > ' + output
+    #---------------------------------------------------------------------------------------  
+    runJob(comm, 'makeBED', flagFile)
+    
+
+def interSectBED(bedFile, outFiles):
+    output, flagFile = outFiles
+    # The input needs to be sorted by readname. Pipe from samtools to HTSeq
+    #------------------------------build shell command--------------------------------------
+    comm = 'pairToBed [OPTIONS] -a ' + bedFile + ' -b ' + output
+    #---------------------------------------------------------------------------------------  
+    runJob(comm, 'interSectBED', flagFile)
