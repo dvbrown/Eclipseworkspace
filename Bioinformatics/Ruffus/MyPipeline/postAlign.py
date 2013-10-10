d040430f92d204fab72a44841eb307bf8e94bf7e
dvbrown <dvbrown@student.unimelb.edu.au>
2013-10-10 17:29:23 +1100

    fixed the piping of htSeq on local comp

--- Bioinformatics/Ruffus/MyPipeline/postAlign.py
+++ Bioinformatics/Ruffus/MyPipeline/postAlign.py
@@ -1,4 +1,6 @@
-refTranscripts = '/vlsci/VR0002/shared/Reference_Files/human_UCSC_genes_v37_nochrprefix.gtf'
+#refTranscripts = '/vlsci/VR0002/shared/Reference_Files/human_UCSC_genes_v37_nochrprefix.gtf'
+
+refTranscripts = '/Users/d.brown6/Documents/public-datasets/annotationFiles/genes.gtf'
 rRnaBedFile = 'someFile' #Find the rRNA bed file
 from tasks import runJob
 
@@ -24,7 +26,8 @@ def htSeq(bamFile, outFiles):
     output, flagFile = outFiles
     # The input needs to be sorted by readname. Pipe from samtools to HTSeq
     #------------------------------build shell command--------------------------------------
-    headParams = 'samtools sort -no ' + bamFile + ' - | samtools view -h |'
+    #remove -u command if uncompressed bam is not valid in sort
+    headParams = 'samtools sort -no ' + bamFile + ' - | samtools view -h - | '
     tailParams = 'python -m HTSeq.scripts.count  --stranded=yes -m intersection-nonempty'
     midParams = ' - ' + refTranscripts + ' > ' + output
     comm = headParams + midParams + tailParams
