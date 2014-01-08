a1718a6cb2c3bdafc56c9ad724d4b1948b678de0
dvbrown <dvbrown@student.unimelb.edu.au>
2014-01-08 16:31:51 +1100

    go back to fix clinical patient names

---
+++ Bioinformatics/Filtering/meltTCGAGeneFiles.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import csv, sys
+
+# The first dataset is gdac.broadinstitute.org_GBM.Merge_transcriptome__agilentg4502a_07_1__unc_edu__Level_3__unc_lowess_normalization_gene_level__data.Level_3.2013121000.0.0/GBM.transcriptome__agilentg4502a_07_1__unc_edu__Level_3__unc_lowess_normalization_gene_level__data.data.txt
+# The second dataset is gdac.broadinstitute.org_GBM.Merge_transcriptome__ht_hg_u133a__broad_mit_edu__Level_3__gene_rma__data.Level_3.2013121000.0.0/GBM.transcriptome__ht_hg_u133a__broad_mit_edu__Level_3__gene_rma__data.data.txt
+
+# There are also 2 versions of Agilent data. Better check out what this is
+
+def readFile(filenameString):
+    'Reads the input file into a dictionary object where the key is the first row'
+    fileA = open(filenameString, 'U')
+    inputA = csv.reader(fileA, delimiter='\t')
+    data = []
+    for row in inputA:
+        data.append(row)
+    return data
+
+
+agilent = readFile(sys.argv[1])
+sampleNames = agilent[0]
+gem = agilent[2:]
+
+newNames = []
+for name in sampleNames:
+    newName = name[0:12]
+    newNames.append(newName)
+print newNames
