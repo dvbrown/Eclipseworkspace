fcaa56fc9bf5bf4d8756a3ed15040acab73c86b3
dvbrown <dvbrown@student.unimelb.edu.au>
2014-03-24 15:50:05 +1100

    skeleton in place

--- Bioinformatics/Filtering/extractReplicates.py
+++ Bioinformatics/Filtering/extractReplicates.py
@@ -8,31 +8,20 @@ def main():
     parser = argparse.ArgumentParser(description="""Reads an input file that is a linear representation of a 96 well plate and extracts the
     replicate level and binds them in columns""")
     parser.add_argument('-i', '--inputData', required=True, help='''The file that is the linear data from a 96 well experiment''')
-    
     parser.add_argument('-r', '--replication', required=True, help='''The level of replication of the experiment ie 2 or 3 replicates. CURRENTLY WORKS FOR TRIPLICATE''')
     parser.add_argument('-d', '--direction', required=True, help='''The orientation of the replicates. CURRENTLY ONLY WORKS FOR VERTICAL''')
-    
     parser.add_argument('-o', '--outputData', required=False, help='The file you get at the end')
     args = parser.parse_args()
     
-    plateMap = aUsefulFunctionsFiltering.readAfile(args.inputData)
-    # Remove the header of the file
-    #plateMap = plateMap[1:]
-    
-    # Flatten the list of lists data structure using itertools
-    plateMap2 = list(itertools.chain.from_iterable(plateMap))
-    
-    # Write in the well names
-    letters = list(string.ascii_uppercase)
-    letters = letters[0:8]
-    number = range(1, 13)
-    
-    wells = []
-    for letter in letters:
-        for num in number:
-            x = letter + str(num)
-            wells.append(x)
-    
+    data = aUsefulFunctionsFiltering.readAfile(args.inputData)
+
+    # Make a header
+    header = ['wells', 'rep1', 'rep2', 'rep3']
+    print header
+
+    if args.replication == 3:
+        for row in data
+
     # Write out to file if one is provided on command line
     if args.outputData == True:
         w = open(args.outputData, 'w')
@@ -42,9 +31,8 @@ def main():
     ######################################
     
     
-    for well, gene in zip(wells, plateMap2):
-        #writer.writerow(well + '\t' + gene)
-        print well + '\t' + gene
+    for row in data:
+        print row
 
 if __name__ == '__main__':
     main()
