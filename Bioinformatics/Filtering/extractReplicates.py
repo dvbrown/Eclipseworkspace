52f227d8ad74d0f326a66ae11b720dc18590617a
dvbrown <dvbrown@student.unimelb.edu.au>
2014-03-27 13:18:41 +1100

    added functionality for the invasion assay as a command line argument

--- Bioinformatics/Filtering/extractReplicates.py
+++ Bioinformatics/Filtering/extractReplicates.py
@@ -9,15 +9,26 @@ def main():
     replicate level and binds them in columns""")
     parser.add_argument('-i', '--inputData', required=True, help='''The file that is the linear data from a 96 well experiment''')
     parser.add_argument('-r', '--replication', required=True, help='''The level of replication of the experiment ie 2 or 3 replicates. CURRENTLY WORKS FOR TRIPLICATE''')
-    parser.add_argument('-d', '--direction', required=True, help='''The orientation of the replicates. CURRENTLY ONLY WORKS FOR VERTICAL''')
+    parser.add_argument('-f', '--fileType', required=True, help='''Whether this is a fluoro plate reader file (no headers)
+        or a invasion assay data. Options = fluoro or invasion''')
     parser.add_argument('-o', '--outputData', required=False, help='The file you get at the end')
     args = parser.parse_args()
     
-    # Input data
+    # Input data. Take out the header if the data is an invasion assay
     data = aUsefulFunctionsFiltering.readAfile(args.inputData)
-    
+    if args.fileType == 'invasion':
+        header = data[0]
+        data = data[1:]
+    elif args.fileType == 'fluoro':
+        pass
+    else:
+        print 'This file type is not a valid invasion assay or data from a plate reader'
+        
     # Extract the well names for replicate 1. PUT THIS AS A command line argument in the future and collate the 2 replicates into 1
     letters1 = ['B', 'C', 'D']
+    # Convert to lower case if the data is invasion assay
+    if args.fileType == 'invasion':
+        letters1 = ['b', 'c', 'd']          
     rep1 = []
     x = 1
     while x < 13:
@@ -27,6 +38,8 @@ def main():
     
     # Extract replicate names for replicate 2
     letters2 = ['E', 'F', 'G']
+    if args.fileType == 'invasion':
+        letters2 = ['e', 'f', 'g']
     rep2 = []
     x = 1
     while x < 13:
@@ -43,14 +56,19 @@ def main():
     # If the data is in triplicate
     if args.replication == '3':
         for row in data:
+            wellName = row[0]
+            # Strip the file extension from the invasion well names
+            if wellName.endswith('.tif'):
+                wellName = wellName[:-4]
+                
             # Search the well name of the data against the list of lists containing the replicate structure
             for group in rep1:
-                if row[0] in group:
+                if wellName in group:
                     # Append to the replicate structure list
                     group.append(row[1])
-            # Repeat for the second group of replicates although this is not necessary        
+            # Repeat for the second group of replicates although one day I will combine this step    
             for group in rep2:
-                if row[0] in group:
+                if wellName in group:
                     group.append(row[1])
 ###################################################### ###################################################### 
     # Some error messages
@@ -68,11 +86,15 @@ def main():
         writer.writerow(zip(wells, plateMap2))
         ##################################
     
-# Print the output to file   
+# Print the output to file
+    if args.fileType == 'invasion':
+        rep1 = rep1[0:4]
+
     for line in rep1:
         print '\t'.join(line)
-    for line in rep2:
-        print '\t'.join(line)
+    if args.fileType == 'fluoro':
+        for line in rep2:
+            print '\t'.join(line)
 # Good old boiler plate
 if __name__ == '__main__':
     main()
