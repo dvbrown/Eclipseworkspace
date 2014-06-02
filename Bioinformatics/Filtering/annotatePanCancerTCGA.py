fc1db5e1b9e4b3cf091747e36141533207dbf4eb
dvbrown <dvbrown@student.unimelb.edu.au>
2014-06-02 13:26:12 +1000

    changed script for breast cancer

--- Bioinformatics/Filtering/annotatePanCancerTCGA.py
+++ Bioinformatics/Filtering/annotatePanCancerTCGA.py
@@ -7,19 +7,21 @@ def main():
         to yield the column letter.""")
     parser.add_argument('-i', '--genomicData', required=True, help='''The file containing elements you want to change. 
         The input file should just contain data and no column/ row headers etc''')
-    parser.add_argument('-m', '--diseaseMap', required=False, help='The map that describes disease codes')
+    parser.add_argument('-c', '--clinicalData', required=True, help='The file that contains the metadata')
     parser.add_argument('-o', '--outputData', required=False, help='The file you get at the end')
     args = parser.parse_args()
     
     rawTable = aUsefulFunctionsFiltering.readAfile(args.genomicData)
-    diseaseCode = aUsefulFunctionsFiltering.readAfile(args.diseaseMap)[1:]
+    diseaseCode = aUsefulFunctionsFiltering.readAfile(args.clinicalData)[1:]
     
     # Initialise the dictionary and read in patient as key and the tumour type as the value
     diseaseDict = {}
     for entry in diseaseCode:
         k = entry[0]
         # The column that specifies tumour or normal
-        v = entry[97]
+#        v = entry[97]
+        # Breast cancer
+        v = entry[149]
         diseaseDict[k] = v
     
     # Extract the header of the genomic file which is the patient
@@ -34,12 +36,12 @@ def main():
     # Change the phenotype labels to be one word
     fixedPhenotype = []
     for case in phenotype:
-        if case == 'Recurrent Tumor':
-            case = 'Recurrent_Tumor'
+        if case == 'Solid Tissue Normal':#'Recurrent Tumor':
+            case = 'Normal'
         elif case == 'Primary Tumor':
             case = 'Primary_Tumor'
-        elif case == 'Solid Tissue Normal':
-            case = 'Normal'
+#        elif case == 'Solid Tissue Normal':
+#            case = 'Normal'
         else:
             case = 'NA'
         fixedPhenotype.append(case)
@@ -52,10 +54,11 @@ def main():
     classNo = len(sampleAnnotation) 
     
     # Emit output
+    
     print str(sampleNo) + '\t' + str(classNo) + '\t1'
     #print '#\t' + '\t'.join(sampleAnnotation)
     # The class labels need to be in order
-    print '#\t' + '\tPrimary_Tumor\tNormal\tRecurrent_Tumor' 
+    print '#\t' + '\tPrimary_Tumor\tNormal'#\tRecurrent_Tumor' 
     print '\t'.join(fixedPhenotype)
     
 if __name__ == '__main__':
