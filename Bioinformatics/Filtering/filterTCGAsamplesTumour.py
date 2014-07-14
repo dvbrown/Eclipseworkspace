d193cfdcef1c66655928b177e22679ecd6f3dbbf
dvbrown <dvbrown@student.unimelb.edu.au>
2014-07-14 16:25:36 +1000

    wrote a subsetting function for TCGA columns

--- Bioinformatics/Filtering/filterTCGAsamplesTumour.py
+++ Bioinformatics/Filtering/filterTCGAsamplesTumour.py
@@ -4,16 +4,28 @@ import argparse
 import pandas as pd
 
 def main():
+    '''Takes a gct file formatted for GSEA and returns only the primary tumours
+     as a gene expression matrix'''
     parser = argparse.ArgumentParser(description="""Reads a gene expression matrix from the TCGA and returns only the primary tumour cases""")
     parser.add_argument('-i', '--genomicData', required=True, help='''The file the gene expression matrix''')
     parser.add_argument('-o', '--outputData', required=False, help='The file you get at the end')
     args = parser.parse_args()
     
-    gem = pd.read_csv(args.genomicData, sep='\t')
-    # Use this syntax to extract columns
-    print gem.loc[:,['sample1', 'sample2']]
+    gem = pd.read_csv(args.genomicData, sep='\t', skiprows=2, index_col=0)
     
-    gem.pd.to_csv(sep="\t")
+    # reguar expression to extract primary tumour farom patients.
+    # last 2 strings will be '01'
+    header = list(gem.columns.values)
+    
+    primaryTumour = []
+    for p in header:
+        tissue = p[12:15]
+        if tissue == '-01':
+            primaryTumour.append(p)
+            
+    # Use this syntax to extract columns
+    result = gem.loc[:,primaryTumour]
+    result.to_csv(args.outputData, sep='\t')
     
     
 if __name__ == '__main__':
