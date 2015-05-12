21c01d1a1144f37043b1a9aa62789146c72a9d54
dvbrown <dvbrown@student.unimelb.edu.au>
2015-05-12 11:40:55 +1000

    run script from current directory to fix

--- Bioinformatics/Filtering/squaashDataMunging.py
+++ Bioinformatics/Filtering/squaashDataMunging.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2.7
 #A script to parse 384 well files and transpose to column vector
 
-import argparse, string, itertools, csv
+import argparse, csv, os
 
 def readAfile(filenameString):
     'Reads the input file into a dictionary object where the key is the first row'
@@ -21,18 +21,48 @@ def writeAfile(fileName, data2Bwritten):
         writer.writerow(row)
     w.close()
     
-    
-    
-    
-    
-    
-    
-    
+      
 def main():
     parser = argparse.ArgumentParser(description="""
     Reads an input file that is output from the imageJ colocalisation plugin 'Squaash'. The data takes all the files put in a directory from
     different imaging fields and combines them into a usable form for analysis in R.
     """)
-    parser.add_argument('-i', '--inputData', required=True, help='''The file that is the raw data from one field analysed by Squaash.''')
+    parser.add_argument('-f', '--inputFolder', required=True, help='''The folder that contains the files from each field analysed by Squaash.''')
+    #parser.add_argument('-i', '--inputData', required=True, help='''The file that is the raw data from one field analysed by Squaash.''')
+    parser.add_argument('-s', '--slideName', required=False, help='The name of the slide used in the immunofluorescence. Will be used to add a filename to the row.')
     parser.add_argument('-o', '--outputData', required=False, help='The csv file you get at the end')
     args = parser.parse_args()
+    
+    # Parse commandline arguments
+    slideName = args.slideName
+    dir = os.getcwd()
+    folder = os.listdir(args.inputFolder)
+    columnHeader = dir + folder[0]
+    print columnHeader
+    
+    # The container for the output file
+#    outputFile = []
+#    # Insert the column header from the first file in the directory
+#    data = readAfile(file[0])
+#    outputFile.append(data[0])
+#    print folder
+    
+#    for file in folder:
+#        print file
+#        data = readAfile(file)
+#        # Delete the second row which contains only parameters
+#        del data[1]
+    
+    
+#    data = readAfile(args.inputData)
+#    # Remove the second row which contains only parameters
+#    del data[1]
+#    print data
+#    
+#    # Print the data to the standard output
+#    for row in data:
+#        print "\t".join(row)
+       
+# Boiler plate to run code
+if __name__ == '__main__':
+    main()
