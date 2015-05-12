4881fad81ed450f4c590e2ed20e7c9461c204f2e
dvbrown <dvbrown@student.unimelb.edu.au>
2015-05-12 11:53:42 +1000

    happy with functionality. Will cleanup

--- Bioinformatics/Filtering/squaashDataMunging.py
+++ Bioinformatics/Filtering/squaashDataMunging.py
@@ -25,43 +25,34 @@ def writeAfile(fileName, data2Bwritten):
 def main():
     parser = argparse.ArgumentParser(description="""
     Reads an input file that is output from the imageJ colocalisation plugin 'Squaash'. The data takes all the files put in a directory from
-    different imaging fields and combines them into a usable form for analysis in R.
+    different imaging fields and combines them into a usable form for analysis in R. The script must be run in from the directory with the raw data.
+    Usage: squaashDataMunging -s 'Name'
     """)
-    parser.add_argument('-f', '--inputFolder', required=True, help='''The folder that contains the files from each field analysed by Squaash.''')
-    #parser.add_argument('-i', '--inputData', required=True, help='''The file that is the raw data from one field analysed by Squaash.''')
-    parser.add_argument('-s', '--slideName', required=False, help='The name of the slide used in the immunofluorescence. Will be used to add a filename to the row.')
+    parser.add_argument('-s', '--slideName', required=True, help='The name of the slide used in the immunofluorescence. Will be used to add a filename to the row.')
     parser.add_argument('-o', '--outputData', required=False, help='The csv file you get at the end')
     args = parser.parse_args()
     
     # Parse commandline arguments
     slideName = args.slideName
-    dir = os.getcwd()
-    folder = os.listdir(args.inputFolder)
-    columnHeader = dir + folder[0]
-    print columnHeader
+    folder = os.listdir(".")
     
     # The container for the output file
-#    outputFile = []
-#    # Insert the column header from the first file in the directory
-#    data = readAfile(file[0])
-#    outputFile.append(data[0])
-#    print folder
+    outputFile = []
+    # Insert the column header from the first file in the directory
+    data = readAfile(folder[0])
+    outputFile.append(data[0])
     
-#    for file in folder:
-#        print file
-#        data = readAfile(file)
-#        # Delete the second row which contains only parameters
-#        del data[1]
+    for f in folder:
+        data = readAfile(f)
+        # Retain only the last (3rd) row which contains the measurements
+        row = data.pop(2)
+        # Add the image name to the first element of the row
+        row[0] = slideName + "_" + row[0]
+        outputFile.append(row)
     
-    
-#    data = readAfile(args.inputData)
-#    # Remove the second row which contains only parameters
-#    del data[1]
-#    print data
-#    
-#    # Print the data to the standard output
-#    for row in data:
-#        print "\t".join(row)
+    # Print the data to the standard output
+    for row in outputFile:
+        print "\t".join(row)
        
 # Boiler plate to run code
 if __name__ == '__main__':
