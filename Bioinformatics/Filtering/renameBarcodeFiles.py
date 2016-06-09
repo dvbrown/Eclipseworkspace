4efd2a0d72a759949f7518ece3abab158b93c30b
Daniel <daniel.brown@cme.vib-kuleuven.be>
2016-06-09 08:07:58 +0200

    made the dictionary

--- Bioinformatics/Filtering/renameBarcodeFiles.py
+++ Bioinformatics/Filtering/renameBarcodeFiles.py
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-import csv, aUsefulFunctionsFiltering, os
+import aUsefulFunctionsFiltering
+import argparse, os
 
 parser = argparse.ArgumentParser(description="""Reads an input file that is a linear representation of a 96 well plate and extracts the
     replicate level and binds them in columns""")
-    parser.add_argument('-i', '--inputData', required=True, help='''The file that is the input''')
-    parser.add_argument('-o', '--outputData', required=False, help='The file you get at the output')
-    args = parser.parse_args()
+parser.add_argument('-i', '--inputData', required=True, help='''The file that is the input''')
+parser.add_argument('-o', '--outputData', required=False, help='The file you get at the output')
+args = parser.parse_args()
 
 #	Read in files
 data = aUsefulFunctionsFiltering.readAfile(args.inputData)
 
-files = os.listdir()
+files = os.listdir('.')
 #	Turn into a dictionary of the barcode and new filename
-dict = {}
-for k,v in data:
-	dict[k] = v
+dic = {}
+for row in data:
+    dic[row[0]] = row[1]
+ 
+print dic
