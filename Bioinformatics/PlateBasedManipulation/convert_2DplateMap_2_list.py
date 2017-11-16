89689880195ff135c922456688f172b5ecae9737
Daniel <daniel.brown@cme.vib-kuleuven.be>
2017-11-16 15:14:16 +0100

    completed the well reformatting script

--- Bioinformatics/PlateBasedManipulation/convert_2DplateMap_2_list.py
+++ Bioinformatics/PlateBasedManipulation/convert_2DplateMap_2_list.py
@@ -6,45 +6,78 @@ Created on Wed Nov 15 16:47:08 2017
 @author: u0107775
 """
 
-import argparse, collections
+import argparse, csv, string
+import pandas as pd
 
 parser = argparse.ArgumentParser(description="""
     This script parsers the plate layout from cell sorting in 2D format.
     It then creates a list based csv file with the well position, sample name and cell number as columns.
     """)
 parser.add_argument('-i', '--inputFile', required=True, help='''The file with the sort template in 2D format''')
-parser.add_argument('-p', '--plateType', required=True, help='''The type of plate you have. Currently valid values are 96 but one day I will make 384 well''')
-parser.add_argument('-o', '--outputFile', required=True, help='''The name of the output file you want''')
+parser.add_argument('-p', '--plateType', required=False, help='''The type of plate you have. Currently valid values are 96 but one day I will make 384 well''')
+parser.add_argument('-o', '--outputFile', required=False, help='''The name of the output file you want''')
 args = parser.parse_args()
 
 fileInput = args.inputFile
+outFileName = args.outputFile
 
-def read2DplateMap(inputFile):
-    'Read in the xth and yth line of the file which contains sample name and cell number respecitively'
+def readFile(inputFile):
+    'Read input file'
     fileA = open(inputFile, 'U')
-    inputA = csv.reader(fileA, delimiter=",")
+    inputA = csv.reader(fileA, delimiter='\t')
     # Read the whole file into a list
     wholeFile = []
-    print wholeFile
-    # Need to find a way to loop through the nth line of a file
     for line in inputA:
-        sample.append(line[0:])
+        wholeFile.append(line)
+    # Remove the header
+    wholeFile = wholeFile[1:]
+    return wholeFile
 
-    # Initialise a list for sample name and a different list for cell number
-    sampleName = []
-    for line in wholeFile:
-        wholeFile.append(line[1::4]) # Change this number depending on which line is the sample number
-    cellNumber = []
-    for line in wholeFile:
-        wholeFile.append(line[1::4]) # Change this number depending on which line is the sample number
+def read2DplateMap(plateMapList, int_sampleNameSkip=4, int_cellNumberSkip=4):
+    'Take the list of the plate map and return the sample name and cell number separately'
+    # Extract the sample name and cell number from the list
+    stop = len(plateMapList)
+    sampleName = plateMapList[2:stop:int_sampleNameSkip]
+    cellNumber = plateMapList[3:stop:int_cellNumberSkip]
+    # Remove the empty 1st element of each list
+    sampleName = [i[1:] for i in sampleName]
+    cellNumber = [i[1:] for i in cellNumber]
     return sampleName, cellNumber
 
+def makePlateCoordinates():
+    'Make the list object with the plate coordinates'
+    letters = list(string.ascii_uppercase)
+    letters = letters[0:8]
+    number = range(1, 13)
+    wells = []
+    for letter in letters:
+        for num in number:
+            x = letter + str(num)
+            wells.append(x)
+    return wells
 
 # Once I have a list of sample names and a list of cell number zip them together in an ordered dict
-    def zipSample_cellNumber_together(sampleName, cellNumber):
+def zipSample_cellNumber_together(platecoordinates, sampleName, cellNumber):
+    'What this finction does'
+#    ordDict = collections.OrderedDict()
+#    ordDict['Well'] = ['Sample', 'Cell_number']
+    # Flatten the sample name and cell number list
+    flatSample = [item for sublist in sampleName for item in sublist]
+    flatCellnumber = [item for sublist in cellNumber for item in sublist]
+    df = pd.DataFrame(
+    {'Coordinate': platecoordinates,
+     'Identity': flatSample,
+     'Number': flatCellnumber
+    })
+    return df
 
 def main():
-    read2DplateMap(fileInput)
+    plateMap = readFile(fileInput)
+    sample, cellNo = read2DplateMap(plateMap, 4, 4)
+    coordinates = makePlateCoordinates()
+    dataFrame = zipSample_cellNumber_together(coordinates, sample, cellNo)
+    print(dataFrame)
+    dataFrame.to_csv(outFileName)
 
 if __name__ == '__main__':
     main()
